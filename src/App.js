e2ade3ce52bc73caecbb63e4a987c454165fa921
joaopedropiraja <jplp100@hotmail.com>
2023-05-07 15:32:14 -0300

    video 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-const express = require("express"); 
+const express = require("express");
 const rotas = require("./routes");
- 
-const app = express(); 
- 
+const cors = require("cors");
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(rotas);
- 
-module.exports = app; 
-
+app.use("*", (req, res) => {
+  res.status(404).json({ message: `Rota '${req.baseUrl}' não encontrada` });
+});
+module.exports = app;
