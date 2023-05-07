807adb61cfe3ef2a184a26bc27ea078acf888805
joaopedropiraja <jplp100@hotmail.com>
2023-05-07 03:37:23 -0300

    aula 1

--- src/Controllers/UsuarioController.js
+++ src/Controllers/UsuarioController.js
@@ -1,7 +1,6 @@
 const UsuarioModel = require("../Models/UsuarioModel");
 
 class UsuarioController {
-
   async create(req, res) {
     const usuario = await UsuarioModel.create(req.body);
 
@@ -14,23 +13,22 @@ class UsuarioController {
     return res.status(200).json(usuarios);
   }
 
-
   async update(req, res) {
     const { id } = req.params;
 
-    const usuario = await UsuarioModel.findByIdAndUpdate(id, req.body, { new: true })
+    const usuario = await UsuarioModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
     return res.status(200).json(usuario);
   }
-
 
   async delete(req, res) {
     const { id } = req.params;
 
     await UsuarioModel.findByIdAndDelete(id);
 
-    return res.status(200).json({ "mensagem": "Usuario deletado com sucesso!" });
-
+    return res.status(200).json({ mensagem: "Usuario deletado com sucesso!" });
   }
 }
 
---
+++ src/Validators/UsuarioValidator.js
@@ -0,0 +1,48 @@
+const { default: mongoose } = require("mongoose");
+const { z } = require("zod");
+const { validateRequest } = require("zod-express-middleware");
+
+// {
+//     "nome": "João",
+//     "email": "hotmail.com",
+//     "senha": "pamon",
+//     "cargo": "Dev líder",
+//     "status": "Sirius a maior!"
+// }
+
+const create = validateRequest({
+  body: z.object({
+    nome: z.string({ required_error: "O nome é obrigatório" }),
+    email: z
+      .string({ required_error: "O email é obrigatório" })
+      .email("O email é inválido"),
+    senha: z.string({ required_error: "A senha é obrigatória" }),
+    cargo: z.string({ required_error: "O cargo é obrigatório" }),
+    status: z.string({ required_error: "O status é obrigatório" }),
+  }),
+});
+
+const destroy = validateRequest({
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
+  }),
+});
+
+const update = validateRequest({
+  body: z.object({
+    nome: z.string().optional(),
+    email: z.string().email("O email é inválido").optional(),
+    senha: z.string().optional(),
+    cargo: z.string().optional(),
+    status: z.string().optional(),
+  }),
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
+  }),
+});
+
+module.exports = {
+  create,
+  destroy,
+  update,
+};
