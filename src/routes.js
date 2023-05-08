256e97653f66d661a67066995458735e3131d106
joaopedropiraja <jplp100@hotmail.com>
2023-05-08 17:01:13 -0300

    video 9

--- src/routes.js
+++ src/routes.js
@@ -5,24 +5,37 @@ const AuthController = require("./Controllers/AuthController");
 const UsuarioValidator = require("./Validators/UsuarioValidator");
 const SessoesValidator = require("./Validators/SessoesValidator");
 const AuthValidator = require("./Validators/AuthValidator");
+const verificarJwt = require("./Middlewares/verificarJwt");
 
 const rotas = Router();
 
 //USUARIOS
 rotas.post("/usuarios", UsuarioValidator.create, UsuarioController.create);
-rotas.get("/usuarios", UsuarioController.read);
+rotas.get("/usuarios", verificarJwt, UsuarioController.read);
 rotas.delete(
   "/usuarios/:id",
+  verificarJwt,
   UsuarioValidator.destroy,
   UsuarioController.delete
 );
-rotas.put("/usuarios/:id", UsuarioValidator.update, UsuarioController.update);
+rotas.put(
+  "/usuarios/:id",
+  verificarJwt,
+  UsuarioValidator.update,
+  UsuarioController.update
+);
 
 //SESSOES
-rotas.post("/sessoes", SessoesValidator.create, SessoesController.create);
-rotas.get("/sessoes", SessoesController.read);
+rotas.post(
+  "/sessoes",
+  verificarJwt,
+  SessoesValidator.create,
+  SessoesController.create
+);
+rotas.get("/sessoes", verificarJwt, SessoesController.read);
 rotas.delete(
   "/sessoes/:id",
+  verificarJwt,
   SessoesValidator.destroy,
   SessoesController.delete
 );
